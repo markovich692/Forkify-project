199e63aad45a3b493abdbc446ea321a689230f99
Marc Diallo <mark.devguru@outlook.com>
2025-03-27 10:40:17 -0500

    creates the async getRecipe function

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+// b53d8e25-a7be-4355-90ac-e0540c6b11d8
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -12,3 +14,27 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+// const data = await fetch('https://forkify-api.jonas.io').then(data =>
+//   console.log(data)
+// );
+
+console.log('TEXT');
+
+const getRecipe = async function () {
+  try {
+    const data = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    if (!data.ok) throw new Error('Could not get recipe');
+
+    const res = await data.json();
+
+    console.log(res.data.recipe);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getRecipe();
