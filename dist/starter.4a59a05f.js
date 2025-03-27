503f81973a93f0ad251ba5f55204b4f43468f7f1
Marc Diallo <mark.devguru@outlook.com>
2025-03-27 11:47:27 -0500

    creates a new recipe object by removing the underscores from the original recipe object

--- dist/starter.4a59a05f.js
+++ dist/starter.4a59a05f.js
@@ -682,8 +682,20 @@ const showRecipe = async function() {
     try {
         const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
         const data = await res.json();
-        if (!res.ok) throw new Error(data.message);
-        console.log(data.data.recipe);
+        if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+        console.log(res, data);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
     } catch (error) {
         console.error(error);
     }
