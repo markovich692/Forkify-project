75b7aabc2f848cd47c51889f6c60b510a559fd0a
Marc Diallo <mark.devguru@outlook.com>
2025-03-29 22:11:33 -0500

    recreates the recipe object by removing the underscore from property names

--- dist/starter.4a59a05f.js
+++ dist/starter.4a59a05f.js
@@ -692,9 +692,21 @@ const renderSpinner = function(parentElement) {
 // );
 const showRecipe = async function() {
     try {
-        const res = await fetch('https://forkify-api.jonas.io');
-        const data = res.json();
+        const res = await fetch('https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const data = await res.json();
         if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+        let { recipe } = data.data;
+        console.log(recipe);
+        recipe = {
+            cookingTime: recipe.cooking_time,
+            id: recipe.id,
+            imageUrl: recipe.image_url,
+            ingredients: recipe.ingredients,
+            publisher: recipe.publisher,
+            servings: recipe.servings,
+            sourceUrl: recipe.source_url,
+            title: recipe.title
+        };
     } catch (error) {
         console.error(error);
     }
