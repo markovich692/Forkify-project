7f1f804ecea348802b3f0402840ea0ae27b037ed
Marc Diallo <mark.devguru@outlook.com>
2025-03-31 12:13:47 -0500

    creates the state object and the loadRecipe() function that fetches from the forkify api and updates the recipe in the state object and export them both

---
+++ src/js/model.js
@@ -0,0 +1,26 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  const res = await fetch(`https://forkify-api.jonas.io/api/v2/recipes/${id}`);
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+  const { recipe } = data.data;
+
+  state.recipe = {
+    cookingTime: recipe.cooking_time,
+    id: recipe.id,
+    imageUrl: recipe.image_url,
+    ingredients: recipe.ingredients,
+    publisher: recipe.publisher,
+    servings: recipe.servings,
+    sourceUrl: recipe.source_url,
+    title: recipe.title,
+  };
+
+  console.log(recipe);
+};
