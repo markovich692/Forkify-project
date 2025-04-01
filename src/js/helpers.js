1e42087314759e07baa8bc70709707f9349549e6
Marc Diallo <mark.devguru@outlook.com>
2025-04-01 10:52:59 -0500

    creates the getSON(), returns the data and uses it inside of the model

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -5,6 +5,7 @@ export const getJSON = async function (url, id) {
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    return data;
   } catch (error) {
     alert(error);
   }
