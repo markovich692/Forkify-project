cf51bd083cf7d1f57967beb55f3e2485fcad6944
Marc Diallo <mark.devguru@outlook.com>
2025-04-03 09:34:23 -0500

    adds the getQuery() method to the recipeView

---
+++ src/js/views/searchView.js
@@ -0,0 +1,9 @@
+const SearchView = class {
+  #parentElement = document.querySelector('.search');
+
+  getQuery() {
+    return this.#parentElement.querySelector('search_field').value;
+  }
+};
+
+export default new SearchView();
