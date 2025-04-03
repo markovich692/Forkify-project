1d78c9ad66b8394ab546d273781ef33e12b4064b
Marc Diallo <mark.devguru@outlook.com>
2025-04-03 11:25:58 -0500

    creates the #clearSearch mthod and uses it inside the addHandlersearch() to remove search value on submit

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,14 +1,20 @@
 const SearchView = class {
   #parentElement = document.querySelector('.search');
 
+  #clearSearch() {
+    this.#parentElement.querySelector('.search__field').value = '';
+  }
+
   getQuery() {
     return this.#parentElement.querySelector('.search__field').value;
   }
 
   addHandlerSearch(handler) {
-    this.#parentElement.addEventListener('submit', function (e) {
+    this.#parentElement.addEventListener('submit', e => {
       e.preventDefault();
       handler();
+      console.log(this);
+      this.#clearSearch();
     });
   }
 };
