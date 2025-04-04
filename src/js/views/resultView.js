009078a3f98bd20c6f17ef15f035cf61b64dbdc5
Marc Diallo <mark.devguru@outlook.com>
2025-04-04 12:19:17 -0500

    creates a _generateMarkupPreview() method and uses it inside the _generateMarkup() method

--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -5,32 +5,26 @@ const ResultView = class extends View {
   _parentElement = document.querySelector('.search-results');
 
   _generateMarkup() {
-    return this._data
-      .map(el => {
-        return `<li class="preview">
-      <a class="preview__link preview__link--active" href="#${el.id}">
-        <figure class="preview__fig">
-          <img src="src/img/test-1.jpg" alt="Test" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${el.title}</h4>
-          <p class="preview__publisher">${el.publisher}</p>
-          <div class="preview__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-        </div>
-      </a>
-    </li>`;
-      })
-      .join('');
+    return this._data.map(this._generateMarkupPreview).join('');
   }
 
-  renderSearch(result) {
-    this._data = result;
-
-    document.querySelector('.results').insertAdjacentHTML('afterbegin', markup);
+  _generateMarkupPreview(el) {
+    return `<li class="preview">
+    <a class="preview__link preview__link--active" href="#${el.id}">
+      <figure class="preview__fig">
+        <img src="src/img/test-1.jpg" alt="Test" />
+      </figure>
+      <div class="preview__data">
+        <h4 class="preview__title">${el.title}</h4>
+        <p class="preview__publisher">${el.publisher}</p>
+        <div class="preview__user-generated">
+          <svg>
+            <use href="${icons}#icon-user"></use>
+          </svg>
+        </div>
+      </div>
+    </a>
+  </li>`;
   }
 };
 
