7da93faac4055f4583863ff9325163d4cf945a5f
Marc Diallo <mark.devguru@outlook.com>
2025-04-03 20:48:42 -0500

    creates the resultView(), put the renderSearch() as its method, imports it calls it inside the controller controlSearchResult to display the recipes on the side bar

---
+++ src/js/views/resultView.js
@@ -0,0 +1,30 @@
+import icons from 'url:../../img/icons.svg';
+
+const ResultView = class {
+  renderSearch(result) {
+    const markup = result
+      .map(el => {
+        return `<li class="preview">
+        <a class="preview__link preview__link--active" href="#${el.id}">
+          <figure class="preview__fig">
+            <img src="src/img/test-1.jpg" alt="Test" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${el.title}</h4>
+            <p class="preview__publisher">${el.publisher}</p>
+            <div class="preview__user-generated">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>`;
+      })
+      .join('');
+    console.log(icons);
+    document.querySelector('.results').insertAdjacentHTML('afterbegin', markup);
+  }
+};
+
+export default new ResultView();
