12ca10a3128a9568925b4736772d0515bae6ee3c
Marc Diallo <mark.devguru@outlook.com>
2025-04-03 20:29:25 -0500

    creates the rrenderSearch emthod in the SearchView and uses it to display recipes

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,3 +1,5 @@
+import icons from 'url:../../img/icons.svg';
+
 const SearchView = class {
   #parentElement = document.querySelector('.search');
 
@@ -16,6 +18,31 @@ const SearchView = class {
       e.preventDefault();
       handler();
     });
+  }
+
+  renderSearch(result) {
+    const markup = result
+      .map(el => {
+        return `<li class="preview">
+        <a class="preview__link preview__link--active" href="#${el.id}">
+          <figure class="preview__fig">
+            <img src="src/img/test-1.jpg" alt="Test" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${el.title}</h4>
+            <p class="preview__publisher">${el.publisher}</p>
+            <div class="preview__user-generated">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>`;
+      })
+      .join('');
+    console.log(icons);
+    document.querySelector('.results').insertAdjacentHTML('afterbegin', markup);
   }
 };
 
