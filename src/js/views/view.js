ab52998138e75fa17bf40d9a4b808ff9a1807e49
Marc Diallo <mark.devguru@outlook.com>
2025-04-04 00:02:15 -0500

    refactors all view using the View

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,3 +1,57 @@
-const View = class View {};
+import icons from 'url:../../img/icons.svg';
+
+const View = class View {
+  _data;
+
+  render(data) {
+    this._data = data;
+
+    const markup = this._generateMarkup();
+    this._clear();
+    this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _clear() {
+    this._parentEl.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markup = `<div class="spinner">
+                <svg>
+                  <use href="${icons}#icon-loader"></use>
+                </svg>
+              </div>`;
+
+    this._clear();
+    this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `<div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>`;
+    this._clear();
+    this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._successMessage) {
+    const markup = `<div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>`;
+
+    this._clear();
+    this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  }
+};
 
 export default View;
