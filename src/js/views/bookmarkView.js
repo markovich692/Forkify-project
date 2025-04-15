fe20c88cc80d5cfab66459fbe059085e6747b575
Marc Diallo <mark.devguru@outlook.com>
2025-04-15 10:18:28 -0500

    creates the bookmarkView and definess the _parentElement and the _generateMarkup

---
+++ src/js/views/bookmarkView.js
@@ -0,0 +1,22 @@
+import icons from '../../img/icons.svg';
+import View from './view';
+
+class bookmarkView extends View {
+  _parenElement = document.querySelector('.nav');
+
+  _generateMarkup() {
+    return `<li class="preview">
+    <a class="preview__link ${
+      result.id === id ? 'preview__link--active' : ''
+    }          " href="#${result.id}">
+      <figure class="preview__fig">
+        <img src="${result.image}" alt="${result.title}" />
+      </figure>
+      <div class="preview__data">
+        <h4 class="preview__title">${result.title}</h4>
+        <p class="preview__publisher">${result.publisher}</p>
+      </div>
+    </a>
+  </li>`;
+  }
+}
