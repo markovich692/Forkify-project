29b54e71e81e2faea83adcf91d4e03e3e1eb64a7
Marc Diallo <mark.devguru@outlook.com>
2025-04-17 13:03:20 -0500

    selects all of the different elements needed to open/close the upload window

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -2,17 +2,23 @@ import View from './view';
 import icons from '../../img/icons.svg';
 
 class AddRecipeView extends View {
-  _parentElement = document.querySelector('.nav__list');
+  _parentElement = document.querySelector('.upload');
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
 
   addHandlerAddRecipe(handler) {
-    this._parentElement.addEventListener('click', e => {
-      const btn = e.target.closest('.nav__btn--add-recipe');
-
-      if (!btn) return;
-
-      handler();
-    });
+    // this._btnOpen.addEventListener('click', e => {
+    //   if (!e.target) return;
+    //   Array.from(document.querySelectorAll('.hidden')).forEach(elements => {
+    //     elements.classList.remove('hidden');
+    //   });
+    // handler();
+    // });
   }
+
+  _generateMarkup() {}
 }
 
 export default new AddRecipeView();
