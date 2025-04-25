14e2eeeb850205c6ad084df7b96ceeb0b46409ca
Marc Diallo <mark.devguru@outlook.com>
2025-04-25 00:11:50 -0500

    removes Array Challenge

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,24 +128,3 @@ const init = function () {
 };
 
 init();
-
-//ARRAYS CHALLENGE
-
-// You are given an initial 2-value array (x). You will use this to calculate a score.
-
-// If both values in (x) are numbers, the score is the sum of the two. If only one is a number, the score is that number. If neither is a number, return 'Void!'.
-
-// Once you have your score, you must return an array of arrays. Each sub array will be the same as (x) and the number of sub arrays should be equal to the score.
-
-// For example:
-
-// if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
-
-const x = [value1, value2];
-
-let score;
-
-if (Number(x[0]) && Number(x[1])) {
-}
-
-//  if  (Number(x[0]) && Number(x[1]))=>{x[0]+x[1]};
